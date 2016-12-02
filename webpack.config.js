6a911b66bf2033d6eacd4be54fb16cea5da86a94
Eric Fuller <info@glassmoonmedia.com>
2016-12-02 17:15:07 -0500

    Initial, very basic webpack config. Let's keep this as simple as possible!

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+	entry: [
+		'babel-polyfill',
+		'./src/index.js'
+	],
+	output: {
+		path: path.resolve(__dirname, 'public'),
+		publicPath: "/assets",
+		filename: "bundle.js"
+	},
+	module: {
+		loaders: [
+			{test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
+		]
+	},
+	plugins: [
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.NoErrorsPlugin()
+	]
+};
